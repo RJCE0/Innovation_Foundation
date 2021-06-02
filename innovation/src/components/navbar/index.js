93ede57b67617e8c611f65e1f9c8d4c137c901f1
D, Tyrell <tyrell.duku19@imperial.ac.uk>
2021-06-02 15:42:10 +0100

    feat(components): Add navbar component

---
+++ innovation/src/components/navbar/index.js
@@ -0,0 +1,31 @@
+import React from 'react';
+
+const Navbar = () => {
+    return (
+        <>
+            <Nav>
+                <NavLink to="/">
+                    <h1> Innovation </h1>
+                </NavLink>
+                <Bars />
+                <NavMenu>
+                    <NavLink to="/" activeStyle>
+                        Home
+            </NavLink>
+                    <NavLink to="/opportunities" activeStyle>
+                        Opportunities
+            </NavLink>
+                    <NavLink to="/student-blog" activeStyle>
+                        Student Blog
+            </NavLink>
+                    <NavLink to="/contact-us" activeStyle>
+                        Contact Us
+            </NavLink>
+                </NavMenu>
+                <NavBtn>
+                    <NavBtnLink to="/login"> Login </NavBtnLink>
+                </NavBtn>
+            </Nav>
+        </>
+    );
+};

58583dd711d5efa8048efdee288a0f3ea8e78043
D, Tyrell <tyrell.duku19@imperial.ac.uk>
2021-06-02 22:48:07 +0100

    refactor(OpportunityCard): Amend braces

--- innovation/src/components/opportunity-card/OpportunityCard.js
+++ innovation/src/components/opportunity-card/OpportunityCard.js
@@ -11,6 +11,22 @@ class OpportunityCard extends React.Component {
     }
 
     render() {
-        return null
+        return (
+            <Card>
+                <Card.Img variant="top" src={this.props.image_src} />
+                <Card.Body>
+                    <Card.Title> {this.props.title}</Card.Title>
+                    <Card.Text>
+                        {"Description: " + this.props.desc + "\n"}
+                        + {"DATE: " + this.props.date + '\n'}
+                            + { "SALARY: " + this.props.salary + '\n' }
+                        + {"LOCATION: " + this.props.location + '\n'}
+                    </Card.Text>
+                    <Button variant="primary">
+                        Apply!
+                    </Button>
+                </Card.Body>
+            </Card>
+        );
     }
 }

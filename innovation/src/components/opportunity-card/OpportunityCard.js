03d5507b30da9427647dcb3de032a4a5eb17a8ed
D, Tyrell <tyrell.duku19@imperial.ac.uk>
2021-06-02 21:48:42 +0100

    feat(OpportunityCard): Add opp-card class

---
+++ innovation/src/components/opportunity-card/OpportunityCard.js
@@ -0,0 +1,15 @@
+class OpportunityCard extends React.Component {
+    /* Opportunity cards should have a:
+        - description
+        - date
+        - salary
+        - location
+    */
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return 
+    }
+}

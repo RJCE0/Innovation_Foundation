e0bd19085b957218b594eab7577ec60cac74d396
Luqman <lml19@ic.ac.uk>
2021-06-04 12:52:48 +0100

    Added tests for database [aad119]

--- front-end/src/App.test.js
+++ front-end/src/App.test.js
@@ -2,10 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
-test('execution', () => {});
+test('Database data can be successfully retrieved', () => {});
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
 })
+
+test('An update in the database updates internships', () => {})

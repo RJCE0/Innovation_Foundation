0062f282f576aef6211b4bf7ad45d6054dc8e845
Luqman <lml19@ic.ac.uk>
2021-06-04 12:47:36 +0100

    Added test for rendering app [aad119]

--- front-end/src/App.test.js
+++ front-end/src/App.test.js
@@ -1,1 +1,11 @@
-test('Nothing happens', () => {});
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+
+test('execution', () => {});
+
+it('renders without crashing', () => {
+  const div = document.createElement('div');
+  ReactDOM.render(<App />, div);
+  ReactDOM.unmountComponentAtNode(div);
+})

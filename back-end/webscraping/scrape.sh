85dcc969bc0212a75d8632993fee498380032da7
Luqman <lml19@ic.ac.uk>
2021-06-16 13:41:10 +0100

    Updated pip3 to pip in scrape shell file

--- back-end/webscraping/scrape.sh
+++ back-end/webscraping/scrape.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
-apt install python3-pip
-pip3 install -r requirements.txt
+pip install -r requirements.txt
 python3 cleanInternships.py
 python3 GradCrackWebScraper.py
 python3 IndeedWebScraper.py

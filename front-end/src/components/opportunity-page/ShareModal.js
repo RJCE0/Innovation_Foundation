95f65f6cadd20aa3eeec199a3076feb9cda5e260
Dalal, Aayush <aayush.dalal19@imperial.ac.uk>
2021-06-17 20:00:49 +0100

    Changed text to images in the share modal for better user experience. [aad119][tld19][lml19]

--- front-end/src/components/opportunity-page/ShareModal.js
+++ front-end/src/components/opportunity-page/ShareModal.js
@@ -10,48 +10,60 @@ import {
   TwitterIcon,
   TwitterShareButton,
   LinkedinIcon,
-  LinkedinShareButton
-} from "react-share"
+  LinkedinShareButton,
+} from "react-share";
+import CopyImg from "../../img/copy_icon.png";
+import TickGif from "../../img/tick.png";
 
 const clipboardStatement =
   "Take a look at this opportunity I found on the Innovation website!\n";
 
-const ShareModal = ({ handleShareClose, showShare, opportunity, copy, setCopy}) => {
+const ShareModal = ({
+  handleShareClose,
+  showShare,
+  opportunity,
+  copy,
+  setCopy,
+}) => {
   const status = copy ? "Link copied to clipboard!" : "Copy Link";
   const innovationHashtag = "#InnovationFoundation";
   return (
     <>
-      <Modal
-        show={showShare}
-        onHide={handleShareClose}
-        keyboard={false}
-      >
+      <Modal show={showShare} onHide={handleShareClose} keyboard={false}>
         <Modal.Header>
           <Modal.Title> Sharing options for {opportunity.title}: </Modal.Title>
         </Modal.Header>
         <Modal.Body>
-          <FacebookShareButton round
+          <FacebookShareButton
+            round
             url={opportunity.link}
             quote={clipboardStatement + opportunity.title + "\n"}
             hash="#innovation"
           >
-            <FacebookIcon round ></FacebookIcon>
-          </FacebookShareButton> {' '}
-          <TwitterShareButton round
+            <FacebookIcon round></FacebookIcon>
+          </FacebookShareButton>{" "}
+          <TwitterShareButton
+            round
             url={opportunity.link}
             title={clipboardStatement + opportunity.title + "\n\n\n"}
             hashtags={["innovation", "opportunity", "workexp"]}
           >
-            <TwitterIcon round ></TwitterIcon>
-          </TwitterShareButton> {' '}
-          <LinkedinShareButton round
+            <TwitterIcon round></TwitterIcon>
+          </TwitterShareButton>{" "}
+          <LinkedinShareButton
+            round
             source={"Innovation Foundation"}
             url={opportunity.link}
-            summary={clipboardStatement + opportunity.title + "\n\n" + innovationHashtag}
+            summary={
+              clipboardStatement +
+              opportunity.title +
+              "\n\n" +
+              innovationHashtag
+            }
             title="Innovation Foundation"
           >
-            <LinkedinIcon round ></LinkedinIcon>
-          </LinkedinShareButton> {' '}
+            <LinkedinIcon round></LinkedinIcon>
+          </LinkedinShareButton>{" "}
           <Button
             onClick={() => {
               setCopy(true);
@@ -59,10 +71,14 @@ const ShareModal = ({ handleShareClose, showShare, opportunity, copy, setCopy}) 
                 clipboardStatement + opportunity.link
               );
             }}
-            variant={(copy) ?  "success" : "primary"}
+            variant={copy ? "success" : "primary"}
           >
-            {" "}
-            {status}{" "}
+            <img
+              width="35px"
+              height="35px"
+              src={copy ? TickGif : CopyImg}
+              alt="Share Link"
+            />
           </Button>
         </Modal.Body>
       </Modal>

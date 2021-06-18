41de96c2b90a853ba0de00237f6f385703811840
Luqman <lml19@ic.ac.uk>
2021-06-18 03:15:41 +0100

    SQL files created for applications

---
+++ back-end/sql/addApplication.sql
@@ -0,0 +1,9 @@
+INSERT INTO "applications" {
+  "opp_id"
+  "full_name",
+  "email",
+  "mobile",
+  "comments",
+  "cv_uploaded"
+}
+VALUES (${opp_id}, ${full_name}, ${email}, ${mobile}, ${comments}, ${cv_uploaded})
